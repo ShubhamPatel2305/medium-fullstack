f17f40fa39c79d0ac3b4a4e6e060d3f202197ff8
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-12 00:05:22 +0530

    some quote change

--- frontend/src/components/SignUpQuote.tsx
+++ frontend/src/components/SignUpQuote.tsx
@@ -4,9 +4,9 @@ const SignUpQuote = () => {
   return (
     <div>
       <div className="bg-gray-300 h-screen flex flex-col justify-center text-left px-24">
-        <div className="text-xl xl:text-2xl font-bold ">Lorem iopsnvjlsnv skv dk vkd vknbvk vkd kd vkbnvoenvks k veovnvnbvln lorem iopsnvjlsnv skv dk vkd vknbvk vkd kd vkbnvoenvks k veovnvnbvln</div>
-        <div className="text-lg font-bold  pt-4">Jules Winfield</div>
-        <div className="tex-lg text-gray-500 ">CEO, Acne Inc</div>
+        <div className="text-xl xl:text-2xl font-bold ">Every journey begins with a single step, and today, you're taking yours towards something greater. Welcome to the place where your ideas find their voice.</div>
+        <div className="text-lg font-bold  pt-4">Shubham Patel</div>
+        <div className="tex-lg text-gray-500 ">Founder, Medium Inc</div>
       </div>
     </div>
   )
